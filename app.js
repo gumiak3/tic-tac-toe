3aac96c5df3d0d452fb66b3c45d1defb47790360
unknown <gumiakk1234@gmail.com>
2022-08-03 11:34:05 +0200

    all mechs are done, but still appearance need to be fixed

---
+++ app.js
@@ -0,0 +1,94 @@
+"use strict";
+const field = document.querySelectorAll(".field");
+const player = document.querySelector(".turn");
+const resetBtn = document.querySelector(".reset-btn");
+console.log(field);
+
+// switching player o => x ; x=>o
+const switchPlayer = (fieldIndex) => {
+  turn === "x" ? (turn = "o") : (turn = "x");
+  player.textContent = `player ${turn}`;
+};
+
+// check if someone won
+const checkWinner = () => {
+  const checkSum = (sum) => {
+    if (sum === 2) {
+      isRunning = false;
+      console.log(`${turn} won!`);
+    }
+  };
+  // draw check
+  let draw = 0;
+  for (let i = 0; i < field.length; i++) {
+    if (field[i].classList.contains("x") || field[i].classList.contains("o")) {
+      draw++;
+    }
+  }
+  if (draw === 9) {
+    console.log("draw");
+    return -1;
+  }
+  // horizontal check
+  for (const [index, values] of points.entries()) {
+    let sum = 0;
+    for (let i = 0; i < values.length; i++) {
+      if (values[i] === values[i + 1]) sum++;
+    }
+    checkSum(sum);
+  }
+  // vertical check
+  for (let i = 0; i < 3; i++) {
+    let sum = 0;
+    for (let j = 0; j < 2; j++) {
+      if (points[j][i] === points[j + 1][i]) sum++;
+    }
+    checkSum(sum);
+  }
+  // diagonally check
+  if (
+    (points[0][0] === points[1][1] && points[1][1] === points[2][2]) ||
+    (points[0][2] === points[1][1] && points[1][1] === points[2][0])
+  ) {
+    isRunning = false;
+    console.log(`${turn} won!`);
+  }
+};
+
+const init = function () {
+  turn = "x";
+  isRunning = true;
+  points = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+  for (let i = 0; i < field.length; i++) {
+    field[i].classList.remove("x");
+    field[i].classList.remove("o");
+  }
+  console.log(points);
+};
+// initialization
+let points = [];
+let turn, isRunning;
+init();
+// click mech
+for (let i = 0; i < field.length; i++) {
+  field[i].addEventListener("click", () => {
+    if (isRunning) {
+      for (let k = 0; k < 3; k++) {
+        for (let j = 0; j < 3; j++) {
+          if (points[k][j] === i) {
+            points[k][j] = `${turn}`;
+            field[i].classList.add(`${turn}`);
+            checkWinner();
+            switchPlayer();
+          }
+        }
+      }
+    }
+  });
+}
+
+resetBtn.addEventListener("click", init);
---
+++ app2.js
@@ -0,0 +1,81 @@
+"use strict";
+
+const resetBtn = document.querySelector(".reset-btn");
+const field = document.querySelectorAll(".field");
+const playerLabel = document.querySelector(".turn");
+const gameState = document.querySelector(".game-container");
+const winnerLabel = document.querySelector(".winner");
+
+const winsPosibility = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const init = function () {
+  turn = "x";
+  isRunning = true;
+  for (let i = 0; i < 9; i++) positions[i] = "";
+  field.forEach((e) => {
+    e.classList.remove("x");
+    e.classList.remove("o");
+  });
+  gameState.classList.remove("win");
+};
+
+const switchPlayer = function () {
+  if (isRunning) {
+    turn = turn === "x" ? (turn = "o") : (turn = "x");
+    // playerLabel.textContent = `player ${turn}`;
+  }
+};
+
+const checkWinner = function () {
+  if (positions.indexOf("") === -1) {
+    console.log("It's a draw!");
+    isRunning = false;
+    return -1;
+  }
+  for (const [index, posibilities] of winsPosibility.entries()) {
+    const [a, b, c] = posibilities;
+    if (
+      positions[a] === positions[b] &&
+      positions[b] === positions[c] &&
+      positions[a] !== "" &&
+      positions[b] !== "" &&
+      positions[c] !== ""
+    ) {
+      isRunning = false;
+      console.log(`PLAYER ${turn} WON! 🎈!`);
+      gameState.classList.add("win");
+      winnerLabel.textContent = turn;
+      return -1;
+    }
+  }
+};
+
+// click mech
+for (let i = 0; i < field.length; i++) {
+  field[i].addEventListener("click", () => {
+    // check if position is empty to avoid collisions and also check if the game isn't done
+    if (positions[i] === "" && isRunning) {
+      positions[i] = turn;
+      field[i].classList.add(turn);
+      checkWinner();
+      switchPlayer();
+    }
+  });
+}
+
+// restart game mech
+resetBtn.addEventListener("click", init);
+//initialization
+let turn, isRunning;
+const positions = [];
+
+init();
